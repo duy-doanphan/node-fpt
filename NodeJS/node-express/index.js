9ca52a1691b200caf84f9af3498ab44d4a6cf118
phanduy <gitphanthduy218@gmail.com>
2023-06-06 13:42:58 +0700

    AS1 task 1

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -10,63 +10,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
 const nationRouter = require('./routes/nationRouter');
-app.use('/nations', nationRouter);
-
-
-// app.all('/nations', (req, res, next) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     next();
-// });
-//
-// app.get('/nations', (req, res, next) => {
-//     res.end('Will send all the nations to you!');
-// });
-//
-// app.post('/nations', (req, res, next) => {
-//     res.end('Will add the nation: ' + req.body.name + ' with details: ' + req.body.description);
-// });
-//
-// app.put('/nations', (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end('PUT operation not supported on /nations');
-// });
-//
-// app.delete('/nations', (req, res, next) => {
-//     res.end('Deleting all nations');
-// });
-//
-// app.get('/nations/:nationId', (req, res, next) => {
-//     res.end('Will send details of the nation: ' + req.params.nationId + ' to you!');
-// });
-//
-// app.post('/nations/:nationId', (req, res, next) => {
-//     res.statusCode = 403;
-//     res.end('POST operation not supported on /nations/' + req.params.nationId);
-// });
-//
-// app.put('/nations/:nationId', (req, res, next) => {
-//     res.write('Updating the nation: ' + req.params.nationId + '\n');
-//     res.end('Will update the nation: ' + req.body.name +
-//         ' with details: ' + req.body.description);
-// });
-//
-// app.delete('/nations/:nationId', (req, res, next) => {
-//     res.end('Deleting nation: ' + req.params.nationId);
-// });
-
-
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
+app.use("/nations", nationRouter);
 
 const server = http.createServer(app);
 
--- NodeJS/node-express/routes/nationRouter.js
+++ NodeJS/node-express/routes/nationRouter.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const nationRouter = express.Router();
-
 nationRouter.use(bodyParser.json());
 
-nationRouter.route('/')
+nationRouter.route("/")
     .all((req, res, next) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader("Content-Type", "text/plain");
         next();
     })
     .get((req, res, next) => {
+        res.statusCode = 200;
         res.end('Will send all the nations to you!');
     })
     .post((req, res, next) => {
@@ -23,6 +23,22 @@ nationRouter.route('/')
     })
     .delete((req, res, next) => {
         res.end('Deleting all nations');
+    });
+
+nationRouter.route("/:nationId")
+    .get((req, res, next) => {
+        res.end('Will send details of the nation: ' + req.params.nationId + ' to you!');
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end("POST operation not supported on /nations/" + req.params.nationId);
+    })
+    .put((req, res, next) => {
+        res.write('Updating the nation: ' + req.params.nationId + '\n');
+        res.end('Will update the nation: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .delete((req, res, next) => {
+        res.end('Deleting nation: ' + req.params.nationId);
     });
 
 module.exports = nationRouter;
