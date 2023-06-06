4fd0881c644b2e685604e326505e5440eefe4c2a
phanduy <gitphanthduy218@gmail.com>
2023-06-06 23:18:34 +0700

    Express REST API with MongoDB and Mongoose Part 1 - exc13 fix

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -1,27 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes');
-var usersRouter = require('./routes/users');
+const hostname = 'localhost';
+const port = 2001;
 
-const mongoose = require('mongoose');
-const Dishes = require('./models/dish');
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
 
-const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
-
-connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => {
-    console.log(err);
-});
-
+const dishRouter = require('./routes/dishRouter')
 
 var app = express();
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://127.0.0.1:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,27 +31,33 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/dishes', dishRouter);
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, hostname, function(){
+  console.log('Server running at http://'+hostname+':'+port);
 });
 
 module.exports = app;
