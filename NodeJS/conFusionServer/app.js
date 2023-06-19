d098fb21fa33df39b5adf79925191d8703130409
Phan Duy <phanduy@Phans-MacBook-Pro.local>
2023-06-19 21:31:29 +0700

    exc15

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -60,6 +60,42 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(cookieParser('12345-67890'));
+function auth (req, res, next) {
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+      res.cookie('user','admin',{signed: true});
+      next(); // authorized
+    } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+    }
+  }
+  else {
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
+    }
+  }
+}
+
 app.listen(port, hostname, function(){
   console.log('Server running at http://'+hostname+':'+port);
 });
